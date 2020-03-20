f840f2f8324759c89346c69904f14bdc155f5de7
Kothing <39289309+kothing@users.noreply.github.com>
2020-03-20 19:31:43 +0800

    Create ControlledTree.jsx

---
+++ src/views/ShowView/Tree/components/ControlledTree.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Tree } from 'antd';
+
+const treeData = [
+    {
+        title: '0-0',
+        key: '0-0',
+        children: [
+            {
+                title: '0-0-0',
+                key: '0-0-0',
+                children: [
+                    {
+                        title: '0-0-0-0',
+                        key: '0-0-0-0',
+                    },
+                    {
+                        title: '0-0-0-1',
+                        key: '0-0-0-1',
+                    },
+                    {
+                        title: '0-0-0-2',
+                        key: '0-0-0-2',
+                    },
+                ],
+            },
+            {
+                title: '0-0-1',
+                key: '0-0-1',
+                children: [
+                    {
+                        title: '0-0-1-0',
+                        key: '0-0-1-0',
+                    },
+                    {
+                        title: '0-0-1-1',
+                        key: '0-0-1-1',
+                    },
+                    {
+                        title: '0-0-1-2',
+                        key: '0-0-1-2',
+                    },
+                ],
+            },
+            {
+                title: '0-0-2',
+                key: '0-0-2',
+            },
+        ],
+    },
+    {
+        title: '0-1',
+        key: '0-1',
+        children: [
+            {
+                title: '0-1-0-0',
+                key: '0-1-0-0',
+            },
+            {
+                title: '0-1-0-1',
+                key: '0-1-0-1',
+            },
+            {
+                title: '0-1-0-2',
+                key: '0-1-0-2',
+            },
+        ],
+    },
+    {
+        title: '0-2',
+        key: '0-2',
+    },
+];
+
+/**
+ * 受控Tree
+ */
+const ControlledTree = () => {
+    const [expandedKeys, setExpandedKeys] = useState(['0-0-0', '0-0-1']);
+    const [checkedKeys, setCheckedKeys] = useState(['0-0-0']);
+    const [selectedKeys, setSelectedKeys] = useState([]);
+    const [autoExpandParent, setAutoExpandParent] = useState(true);
+
+    const onExpand = expandedKeys => {
+        console.log('onExpand', expandedKeys); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+        // or, you can remove all expanded children keys.
+
+        setExpandedKeys(expandedKeys);
+        setAutoExpandParent(false);
+    };
+
+    const onCheck = checkedKeys => {
+        console.log('onCheck', checkedKeys);
+        setCheckedKeys(checkedKeys);
+    };
+
+    const onSelect = (selectedKeys, info) => {
+        console.log('onSelect', info);
+        setSelectedKeys(selectedKeys);
+    };
+
+    return (
+        <Tree
+            checkable
+            onExpand={onExpand}
+            expandedKeys={expandedKeys}
+            autoExpandParent={autoExpandParent}
+            onCheck={onCheck}
+            checkedKeys={checkedKeys}
+            onSelect={onSelect}
+            selectedKeys={selectedKeys}
+            treeData={treeData}
+        />
+    );
+};
+
+export default ControlledTree;
