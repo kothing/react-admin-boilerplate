c2e112e96a5f5b0185f2c59f5b564174e0facb9f
Kothing <39289309+kothing@users.noreply.github.com>
2020-03-20 19:20:26 +0800

    Update AppHeader.jsx

--- src/containers/AppHeader.jsx
+++ src/containers/AppHeader.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Menu, Dropdown, Icon, Layout, Avatar, Badge } from 'antd'
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Menu, Dropdown, Layout, Avatar, Badge } from 'antd';
+import { BellOutlined, EditOutlined, GithubOutlined, LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, SettingOutlined } from '@ant-design/icons';
 const { Header } = Layout
 
 const AppHeader = props => {
@@ -11,18 +11,16 @@ const AppHeader = props => {
             <Menu.ItemGroup title='用户设置'>
                 <Menu.Divider />
                 <Menu.Item>
-                    <Icon type='edit' />
-                    个人设置
+                    <EditOutlined /> 个人设置
                 </Menu.Item>
                 <Menu.Item>
-                    <Icon type='setting' theme='filled' />
-                    系统设置
+                    <SettingOutlined /> 系统设置
                 </Menu.Item>
             </Menu.ItemGroup>
             <Menu.Divider />
             <Menu.Item>
                 <span onClick={loginOut}>
-                    <Icon type='logout' /> 退出登录
+                    <LogoutOutlined /> 退出登录
                 </span>
             </Menu.Item>
         </Menu>
@@ -30,29 +28,30 @@ const AppHeader = props => {
     return (
         <Header className='header'>
             <div className='left'>
-                <Icon
-                    style={{ fontSize: '2rem' }}
-                    onClick={menuClick}
-                    type={menuToggle ? 'menu-unfold' : 'menu-fold'}
-                />
+                {
+                    menuToggle
+                    ? <MenuUnfoldOutlined onClick={menuClick}/>
+                    : <MenuFoldOutlined onClick={menuClick}/>
+                }
             </div>
             <div className='right'>
                 <div className='mr15'>
                     <a rel='noopener noreferrer' href='/' target='_blank'>
-                        <Icon type='github' style={{ color: '#000' }} />
+                        <GithubOutlined style={{ color: '#000' }}/>
                     </a>
                 </div>
                 <div className='mr15'>
                     <Badge dot={true} offset={[-2, 0]}>
                         <a href='/' style={{ color: '#000' }}>
-                            <Icon type='bell' />
+                            <BellOutlined />
                         </a>
                     </Badge>
                 </div>
                 <div>
                     <Dropdown overlay={menu} overlayStyle={{ width: '20rem' }}>
                         <div className='ant-dropdown-link'>
-                            <Avatar icon='user' src={avatar} alt='avatar' style={{ cursor: 'pointer' }} />
+                            {/* <Avatar icon='user' src={avatar} alt='avatar' style={{ cursor: 'pointer' }} /> */}
+                            <Avatar src={avatar} alt='avatar' style={{ cursor: 'pointer' }} />
                         </div>
                     </Dropdown>
                 </div>
