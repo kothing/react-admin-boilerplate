99957ffe63a9a135094480961a59ac669b9b0fb1
Kothing <39289309+kothing@users.noreply.github.com>
2020-03-20 19:26:00 +0800

    Update Login.jsx

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,54 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import { Layout, Input, Icon, Form, Button, Divider, message, notification } from 'antd'
-import { withRouter } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Layout, Input, Form, Button, Divider, message, notification } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 // import axios from '@/api'
 // import { API } from '@/api/config'
 import '@/style/view-style/login.scss'
 
 const Login = props => {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
 
-    const { getFieldDecorator } = props.form
+    const handleSubmitFinish = values => {
+        console.log('Success:', values);
+            // let { username, password } = values
+            // axios
+            //     .post(`${API}/login`, { username, password })
+            //     .then(res => {
+            //         if (res.data.code === 0) {
+            //             localStorage.setItem('user', JSON.stringify(res.data.data.user))
+            //             localStorage.setItem('token', res.data.data.token)
+            //             props.history.push('/')
+            //             message.success('登录成功!')
+            //         } else {
+            //             message.error('登录失败!')
+            //         }
+            //     })
+            //     .catch(err => {
+            //         message.error('登录失败!')
+            //     })
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        props.form.validateFields((err, values) => {
-            if (!err) {
-                // let { username, password } = values
-                // axios
-                //     .post(`${API}/login`, { username, password })
-                //     .then(res => {
-                //         if (res.data.code === 0) {
-                //             localStorage.setItem('user', JSON.stringify(res.data.data.user))
-                //             localStorage.setItem('token', res.data.data.token)
-                //             props.history.push('/')
-                //             message.success('登录成功!')
-                //         } else {
-                //             message.error('登录失败!')
-                //         }
-                //     })
-                //     .catch(err => {
-                //         message.error('登录失败!')
-                //     })
-
-                // 这里可以做权限校验 模拟接口返回用户权限标识
-                switch (values.username) {
-                    case 'admin':
-                        values.auth = 0
-                        break
-                    default:
-                        values.auth = 1
-                }
-
-                localStorage.setItem('user', JSON.stringify(values))
-                setLoading(true)
-                setTimeout(() => {
-                    message.success('登录成功!')
-                    props.history.push('/')
-                }, 2000)
+            // 这里可以做权限校验 模拟接口返回用户权限标识
+            switch (values.username) {
+                case 'admin':
+                    values.auth = 0
+                    break
+                default:
+                    values.auth = 1
             }
-        })
-    }
+
+            localStorage.setItem('user', JSON.stringify(values))
+            setLoading(true)
+            setTimeout(() => {
+                message.success('登录成功!')
+                props.history.push('/')
+            }, 2000);
+    };
+    
+    const handleSubmitFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
 
     useEffect(() => {
         notification.open({
@@ -67,27 +66,30 @@ const Login = props => {
                 <div className='login-form'>
                     <h3>后台管理系统</h3>
                     <Divider />
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Item>
-                            {getFieldDecorator('username', {
-                                rules: [{ required: true, message: '请输入用户名!' }]
-                            })(
-                                <Input
-                                    prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    placeholder='用户名'
-                                />
-                            )}
+                    <Form
+                        onFinish={handleSubmitFinish}
+                        onFinishFailed={handleSubmitFinishFailed}
+                    >
+                        <Form.Item
+                            // label="Username"
+                            name="username"
+                            rules={[{ required: true, message: '请输入用户名' }]}
+                        >
+                            <Input
+                                placeholder='用户名'
+                                prefix={<UserOutlined className="site-form-item-icon" />}
+                            />
                         </Form.Item>
-                        <Form.Item>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: '请输入密码' }]
-                            })(
-                                <Input
-                                    prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    type='password'
-                                    placeholder='密码'
-                                />
-                            )}
+                        <Form.Item
+                            // label="Password"
+                            name="password"
+                            rules={[{ required: true, message: '请输入密码' }]}
+                        >
+                            <Input.Password
+                                type="password"
+                                placeholder="密码"
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                            />
                         </Form.Item>
                         <Form.Item>
                             <Button type='primary' htmlType='submit' className='login-form-button' loading={loading}>
@@ -101,4 +103,4 @@ const Login = props => {
     )
 }
 
-export default withRouter(Form.create()(Login))
+export default withRouter(Login);
