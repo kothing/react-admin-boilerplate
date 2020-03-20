888871c7e54b770e32c104c1dcb4728b8f315bba
Kothing <39289309+kothing@users.noreply.github.com>
2020-03-20 19:31:23 +0800

    Create BaseTree.jsx

---
+++ src/views/ShowView/Tree/components/BaseTree.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Tree } from 'antd';
+
+
+const treeData = [
+    {
+        title: '0-0',
+        key: '0-0',
+        children: [
+            {
+                title: '0-0-0',
+                key: '0-0-0',
+                children: [
+                    { title: '0-0-0-0', key: '0-0-0-0' },
+                    { title: '0-0-0-1', key: '0-0-0-1' },
+                    { title: '0-0-0-2', key: '0-0-0-2' }
+                ]
+            },
+            {
+                title: '0-0-1',
+                key: '0-0-1',
+                children: [
+                    { title: '0-0-1-0', key: '0-0-1-0' },
+                    { title: '0-0-1-1', key: '0-0-1-1' },
+                    { title: '0-0-1-2', key: '0-0-1-2' }
+                ]
+            },
+            {
+                title: '0-0-2',
+                key: '0-0-2'
+            }
+        ]
+    },
+    {
+        title: '0-1',
+        key: '0-1',
+        children: [
+            { title: '0-1-0-0', key: '0-1-0-0' },
+            { title: '0-1-0-1', key: '0-1-0-1' },
+            { title: '0-1-0-2', key: '0-1-0-2' }
+        ]
+    },
+    {
+        title: '0-2',
+        key: '0-2'
+    }
+]
+
+/**
+ * 基础组件
+ */
+const BaseTree = () => {
+    const onSelect = (selectedKeys, info) => {
+        console.log('selected', selectedKeys, info);
+    };
+
+    const onCheck = (checkedKeys, info) => {
+        console.log('onCheck', checkedKeys, info);
+    };
+
+    return (
+        <Tree
+            checkable
+            defaultExpandedKeys={['0-0-0', '0-0-1']}
+            defaultSelectedKeys={['0-0-0', '0-0-1']}
+            defaultCheckedKeys={['0-0-0', '0-0-1']}
+            onSelect={onSelect}
+            onCheck={onCheck}
+            treeData={treeData}
+        />
+    );
+}
+
+export default BaseTree
