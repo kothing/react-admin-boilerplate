5ba02e406d95e5baeb9f03ff759fdfee381ec89d
Kothing <39289309+kothing@users.noreply.github.com>
2020-05-21 19:23:25 +0800

    Update index.js

--- src/views/TestView/Example/index.js
+++ src/views/TestView/Example/index.js
@@ -1,3 +1,17 @@
-import Example from "./Example.jsx";
+import Example1 from "./Example1.jsx";
+
+const Example = () => {
+  return (
+    <Layout>
+      <div>
+        <WebBreadcrumb arr={["示例"]} />
+      </div>
+      <div className="base-style">
+        <h3>示例</h3>
+        <Example1/>
+      </div>
+    </Layout>
+  );
+};
 
 export default Example;
