fce9cc4d9878572c16f2d5c72605fc080cba79c8
Kothing <39289309+kothing@users.noreply.github.com>
2020-05-21 19:34:43 +0800

    Update Example1.jsx

--- src/views/TestView/Example/Example1.jsx
+++ src/views/TestView/Example/Example1.jsx
@@ -1,6 +1,5 @@
 import React, { useReducer} from "react";
-import { Button, Layout, Divider } from "antd";
-import WebBreadcrumb from "@/components/WebBreadcrumb";
+import { Button } from "antd";
 
 const initialState = {
   past: [],
@@ -53,20 +52,12 @@ function reducer(state, action) {
 const Example1 = () => {
   const [state, dispatch] = useReducer(reducer, initialState, init);
   return (
-    <Layout>
-      <div>
-        <WebBreadcrumb arr={["示例"]} />
-      </div>
       <div className="base-style">
-        <h3>示例</h3>
-        <Divider />
-        <p>这个示例</p>
         <p>{state.present.count}</p>
         {state.past && state.past.length > 0 ? <Button onClick={() => dispatch({ type: "UNDO" })}>后退UNDO</Button> : <Button disabled>后退UNDO</Button>}{" "}
         <Button onClick={() => dispatch({type: "INCREMENT"})}>增加INCREMENT</Button>{" "}
         {state.future && state.future.length > 0 ? <Button onClick={() => dispatch({ type: "REDO" })}>前进REDO</Button> : <Button disabled>前进REDO</Button>}
       </div>
-    </Layout>
   );
 };
 
