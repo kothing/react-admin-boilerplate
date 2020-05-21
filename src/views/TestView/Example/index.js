ec2adfe0be8c2a8935301e7db9c5b7017a3db130
Kothing <39289309+kothing@users.noreply.github.com>
2020-05-21 19:27:08 +0800

    Update index.js

--- src/views/TestView/Example/index.js
+++ src/views/TestView/Example/index.js
@@ -1,4 +1,7 @@
+import { Layout, Divider } from "antd";
+import WebBreadcrumb from "@/components/WebBreadcrumb";
 import Example1 from "./Example1.jsx";
+import Example2 from "./Example2.jsx";
 
 const Example = () => {
   return (
@@ -9,6 +12,8 @@ const Example = () => {
       <div className="base-style">
         <h3>示例</h3>
         <Example1/>
+        <Divider />
+        <Example2/>
       </div>
     </Layout>
   );
