b60c1d746188797696a536b06af5082f8347887e
Kothing <39289309+kothing@users.noreply.github.com>
2020-05-21 19:04:16 +0800

    Update Example.jsx

--- src/views/TestView/Example.jsx
+++ src/views/TestView/Example.jsx
@@ -1,22 +1,18 @@
 import React, { useReducer} from "react";
-import { Layout, Divider } from "antd";
+import { Button, Layout, Divider } from "antd";
 import WebBreadcrumb from "@/components/WebBreadcrumb";
 
 const initialState = {
-  past: [
-    {
-      count: 0
-    }
-  ],
+  past: [],
   present: {
-    count: 1
+    count: 0
   },
   future: []
 };
 
 function init(initialState) {
   return {
-    past: initialState.past,
+    past: [],
     present: initialState.present,
     future: [],
   };
@@ -66,9 +62,9 @@ const Example = () => {
         <Divider />
         <p>这个示例</p>
         <p>{state.present.count}</p>
-        {state.past && state.past.length > 0 ? <button onClick={() => dispatch({ type: "UNDO" })}>后退UNDO</button> : <button disabled>后退UNDO</button>} 
-        <button onClick={() => dispatch({type: "INCREMENT"})}>增加INCREMENT</button> 
-        {state.future && state.future.length > 0 ? <button onClick={() => dispatch({ type: "REDO" })}>前进REDO</button> : <button disabled>前进REDO</button>}
+        {state.past && state.past.length > 0 ? <Button onClick={() => dispatch({ type: "UNDO" })}>后退UNDO</Button> : <Button disabled>后退UNDO</Button>}{" "}
+        <Button onClick={() => dispatch({type: "INCREMENT"})}>增加INCREMENT</Button>{" "}
+        {state.future && state.future.length > 0 ? <Button onClick={() => dispatch({ type: "REDO" })}>前进REDO</Button> : <Button disabled>前进REDO</Button>}
       </div>
     </Layout>
   );
