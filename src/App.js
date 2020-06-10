89e3a6a9692f8b3e193549d9fe6a95fc96c9d94e
Kothing <39289309+kothing@users.noreply.github.com>
2020-06-10 16:57:36 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import AsyncLoadable from '@/utils/AsyncLoadable'
 import 'animate.css'
 import './style/base.less'
