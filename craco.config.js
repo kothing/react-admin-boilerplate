c6c3aa5d44953f21c4554b6c0790a81f2bd01b42
Lyove <lyove@outlook.com>
2023-03-13 18:14:58 +0800

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,32 @@
 
 // const path = require('path');
-const CracoAntDesignPlugin = require("craco-antd");
+// const CracoAntDesignPlugin = require("craco-antd");
+const CracoLessPlugin = require('craco-less');
 const CracoAlias = require("craco-alias");
 
 module.exports = {
     plugins: [
+//         {
+//             plugin: CracoAntDesignPlugin,
+//             options: {
+//                 /* customizeTheme 和 customizeThemeLessPath 任选其一 */
+//                 customizeTheme: {
+//                     "@primary-color": "#7546c9",
+//                     "@link-color": "#7546c9"
+//                 },
+//                 // customizeThemeLessPath: path.join(__dirname, "src/style/theme.less"),
+//             }
+//         },
         {
-            plugin: CracoAntDesignPlugin,
-            options: {
-                /* customizeTheme 和 customizeThemeLessPath 任选其一 */
-                customizeTheme: {
-                    "@primary-color": "#7546c9",
-                    "@link-color": "#7546c9"
-                },
-                // customizeThemeLessPath: path.join(__dirname, "src/style/theme.less"),
-            }
+          plugin: CracoLessPlugin,
+          options: {
+            lessLoaderOptions: {
+              lessOptions: {
+                modifyVars: { '@primary-color': '#1DA57A' },
+                javascriptEnabled: true,
+              },
+            },
+          },
         },
         {
             plugin: CracoAlias,
