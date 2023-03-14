91c4692c701dda6cb7b3c98072f9f4da807e2330
Lyove <lyove@outlook.com>
2023-03-14 17:55:52 +0800

    Update Editor.jsx

--- src/views/Others/Editor/Editor.jsx
+++ src/views/Others/Editor/Editor.jsx
@@ -1,62 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import BraftEditor from 'braft-editor'
-import WebBreadcrumb from '@/components/WebBreadcrumb'
-import { Layout, Divider } from 'antd'
-import '@/style/view-style/editor.less'
-import 'braft-editor/dist/index.css'
+import React from "react";
 
 const EditorView = () => {
-    const [state, setState] = useState({
-        editorState: BraftEditor.createEditorState('<p>你好 <b>世界!</b></p>'),
-        outputHTML: '<p>你好 <b>世界!</b></p>'
-    })
-
-    let { editorState, outputHTML } = state
-
-    useEffect(() => {
-        let timer = setTimeout(() => {
-            setState(prevState => {
-                return {
-                    ...prevState,
-                    editorState: BraftEditor.createEditorState('你好,<b>可爱的人! 很幸运在这里与你相遇!</b>')
-                }
-            })
-        }, 3000)
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [state])
-
-    let editorChange = editorState => {
-        setState(prevState => {
-            return { ...prevState, editorState, outputHTML: editorState.toHTML() }
-        })
-    }
-
     return (
-        <Layout className='animated fadeIn'>
-            <div>
-                <WebBreadcrumb arr={['其他', '富文本']}></WebBreadcrumb>
-            </div>
-            <div className='base-style'>
-                <h3>何时使用</h3>
-                <Divider />
-                <p>
-                    当用户需要一些特定输入时，此页面使用的富文本编辑器是
-                    <a href='https://github.com/margox/braft-editor'>braft-editor</a>
-                </p>
-            </div>
-            <div className='base-style'>
-                <div className='editor'>
-                    <BraftEditor value={editorState} onChange={editorChange} />
-                </div>
-            </div>
-            <div className='base-style'>
-                <h5>输出内容</h5>
-                <div className='output-content'>{outputHTML}</div>
-            </div>
-        </Layout>
-    )
-}
+        <div className="animated fadeIn">
+            <iframe
+                src="https://codesandbox.io/embed/lexical-react-editor-npi9by?fontsize=14&hidenavigation=1&theme=dark"
+                style={{
+                    width: "100%",
+                    height: "800px",
+                    border: 0,
+                    borderRadius: "4px",
+                    overflow: "hidden"
+                }}
+                title="lexical-react-editor"
+                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            />
+        </div>
+    );
+};
 
-export default EditorView
+export default EditorView;
